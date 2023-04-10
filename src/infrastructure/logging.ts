fe7378521363c256d781e47e519b614c7185ae9f
Stephen Mue <8377886+stephen2m@users.noreply.github.com>
2023-04-11 00:41:30 +0300

    Update winston config

--- src/infrastructure/logging.ts
+++ src/infrastructure/logging.ts
@@ -2,11 +2,13 @@ const winston = require('winston');
 import { Logtail } from '@logtail/node';
 import { LogtailTransport } from '@logtail/winston';
 
+const { combine, timestamp, json } = winston.format;
+
 const logtail = new Logtail(process.env.LOGTAIL_TOKEN ?? '');
 
 export const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
+    level: process.env.LOG_LEVEL || 'info',
+    format: combine(timestamp(), json()),
     defaultMeta: { service: 'svix-metrics' },
     transports: [
         new LogtailTransport(logtail)
